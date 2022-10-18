07b9ed2f1cf479fb956243ef3d94489b38b27b45
shivamvinove <110169196+shivamvinove@users.noreply.github.com>
2022-10-18 11:40:10 +0530

    first commit

---
+++ cp1/code.py
@@ -0,0 +1,20 @@
+import sys
+import pdb 
+sys.stdout= open('cp1/output.txt','w')
+sys.stdin= open('cp1/input.txt','r')
+
+#code for testing the unit test case.......
+t = int(input())
+for i in range(t):
+    x,y = map(int,input().split(' '))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
